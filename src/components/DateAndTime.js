fc59cfc586e5179d682d5ac6cf85466f9036e2ae
Constance Reversé <constance.reverse@hotmail.fr>
2018-09-27 11:14:47 +0200

    routes

---
+++ src/components/DateAndTime.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DateAndTime() {
+  return (
+    <section>
+      <h2>Choose your date</h2>
+    </section>
+  );
+}
+
+export default DateAndTime;
---
+++ src/components/MyAccount.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyAccount() {
+  return (
+    <section>
+      <h2>Your account</h2>
+    </section>
+  );
+}
+
+export default MyAccount;
