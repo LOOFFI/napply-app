ec7ca7ece9220897ca5b28c1795ee2e1d97cff54
Constance Reversé <constance.reverse@hotmail.fr>
2018-09-28 16:51:01 +0200

    done

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 import api from "../api.js";
 
@@ -8,37 +8,85 @@ class Location extends React.Component {
     super(props);
 
     this.state = {
-      truck_id: "",
-      user_id: ""
+      truck_id: "truck1",
+      user_id: "",
+      isSubmitSuccess: false,
+      bookingId: ""
     };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const { _id } = this.props.currentUser;
+    this.setState({ user_id: _id }, () => {
+      console.log(this.state);
+      api
+        .post("/location", this.state)
+        .then(response => {
+          this.setState({
+            isSubmitSuccess: true,
+            bookingId: response.data._id
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          alert("Sorry! Something went wrong. 💩");
+        });
+    });
+  }
+
+  updateTruck(event) {
+    const { value } = event.target;
+    this.setState({ truck_id: value });
   }
 
   render() {
     const { currentUser } = this.props;
+    const { isSubmitSuccess, bookingId } = this.state;
 
-    {
-      currentUser && console.log(this.props);
+    if (isSubmitSuccess) {
+      return <Redirect to={`/booking-date/${bookingId}`} />;
     }
 
     return (
       <section>
         <h2>Choose your location</h2>
-        <form>
+        <form onSubmit={event => this.handleSubmit(event)}>
           <label>
-            Truck 1<input type="radio" name="truck_id" value="truck1" checked />
+            Truck 1
+            <input
+              type="radio"
+              name="truck_id"
+              value="truck1"
+              onChange={event => this.updateTruck(event)}
+              checked={this.state.truck_id === "truck1"}
+            />
           </label>
 
           <label>
-            Truck 2<input type="radio" name="truck_id" value="truck2" />
+            Truck 2
+            <input
+              type="radio"
+              name="truck_id"
+              value="truck2"
+              onChange={event => this.updateTruck(event)}
+              checked={this.state.truck_id === "truck2"}
+            />
           </label>
 
           <label>
-            Truck 3<input type="radio" name="truck_id" value="truck3" />
+            Truck 3
+            <input
+              type="radio"
+              name="truck_id"
+              value="truck3"
+              onChange={event => this.updateTruck(event)}
+              checked={this.state.truck_id === "truck3"}
+            />
           </label>
 
-          <button>
-            <Link to="/booking-date"> Next step</Link>
-          </button>
+          <button>Next step</button>
         </form>
       </section>
     );
